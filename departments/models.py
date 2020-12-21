f7dd3ea8161d36081d7208cc4c7891e942fa668e
marcusoyh <ongmarcus204@gmail.com>
2020-12-21 23:07:11 +0800

    added fields to dept model

--- departments/models.py
+++ departments/models.py
@@ -4,8 +4,10 @@ from django.db import models
 class Department(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=1000)
-    #icon = models.
-    #route = models.
+    icon = models.CharField(default='', max_length=1000)
+    route = models.CharField(default='', max_length=1000)
+    purpose = models.TextField(default='')
+    goal = models.TextField(default='')
 
     def __str__(self):
         return self.name
