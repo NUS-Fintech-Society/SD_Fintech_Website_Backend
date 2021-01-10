95596a6baa99af916bab15131e4f2e3dbb0d344e
marcusoyh <ongmarcus204@gmail.com>
2021-01-10 15:19:11 +0800

    endpoints added to views.py

--- site_drf/views.py
+++ site_drf/views.py
@@ -4,11 +4,17 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+
 @api_view(['GET'])
 def apiOverview(request):
-  api_urls = {
-    'List, Create Contact Form Submissions': '/contact/',
-    'View Contact Form Submission Details': '/contact/<id>/',
-  }
+    api_urls = {
+        'List, Create Contact Form Submissions': '/contact/',
+        'View Contact Form Submission Details': '/contact/<id>/',
+        'List, Projects done by the Society': '/projects/',
+        'View Project Details': '/projects/<id>/',
+        'List, Projects done by a specific department': '/projects/department/<deptId>/',
+        'View, all Deparments': '/departments/',
+        'View, Department Details': '/departments/<id>/',
+    }
 
-  return Response(api_urls)
+    return Response(api_urls)
